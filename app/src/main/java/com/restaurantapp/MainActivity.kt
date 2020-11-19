4f5cfc7206639c68d4ed903f3911200310eeda83
zsfelmeri <felemri.zsolt@yahoo.com>
2020-11-19 11:16:56 +0200

    Design and Fragments

---
+++ app/src/main/java/com/restaurantapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.restaurantapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.restaurantapp.databinding.ActivityMainBinding
+import fragments.LoginFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_host_fragment, LoginFragment())
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_profile){
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        if(item.itemId == R.id.action_list){
+            Toast.makeText(this, "List", Toast.LENGTH_SHORT).show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
